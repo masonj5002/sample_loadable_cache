4422ca55a91089e94e3b747477257784e06f6b79
Mason Jennings <masonj@csu.fullerton.edu>
2025-05-16 19:10:58 -0700

    random replacement caching policy updated to not evict until cache is full

--- random_replacement.c
+++ random_replacement.c
@@ -15,6 +15,8 @@ typedef struct {
 
 #define CACHE_SIZE 1000
 CacheEntry cache[CACHE_SIZE];
+int current_cache_size;
+
 #define VALUE_NOT_PRESENT 0
 
 int cache_requests;
@@ -26,9 +28,11 @@ ProviderFunction _downstream = NULL;
 void initialize(void) {
     fprintf(stderr, __FILE__ " initialize()\n");
     srand(time(NULL));
-    cache_requests = 0;
-    cache_hits     = 0;
-    cache_misses   = 0;
+
+    current_cache_size = 0;
+    cache_requests     = 0;
+    cache_hits         = 0;
+    cache_misses       = 0;
 
     for (int ix = 0; ix < CACHE_SIZE; ix++)
         cache[ix].valid = VALUE_NOT_PRESENT;
@@ -41,11 +45,11 @@ void cleanup(void) {
 CacheStat* statistics(void) {
     fprintf(stderr, __FILE__ " statistics()\n");
 
-    CacheStat* current_stats = malloc(4 * sizeof(CacheStat));
+    CacheStat* current_stats = malloc(5 * sizeof(CacheStat));
     current_stats[0]         = (CacheStat){Cache_requests, cache_requests};
     current_stats[1]         = (CacheStat){Cache_hits, cache_hits};
     current_stats[2]         = (CacheStat){Cache_misses, cache_misses};
-    current_stats[3]         = (CacheStat){Cache_size, CACHE_SIZE};
+    current_stats[3]         = (CacheStat){Cache_size, current_cache_size};
     current_stats[4]         = (CacheStat){END_OF_STATS, 0};
 
     return current_stats;
@@ -58,47 +62,65 @@ void reset_statistics(void) {
     cache_misses   = 0;
 }
 
-bool _is_present(KeyType key) {
-    bool present = false;
-    for (int ix = 0; ix < CACHE_SIZE; ix++)
-        if (cache[ix].valid && cache[ix].key == key)
-            present = true;
-    fprintf(stderr, __FILE__ " is_present(" KEY_FMT ") = %s \n", key,
-            present ? "true" : "false");
-    return present;
+// bool _is_present(KeyType key) {
+//     bool present = false;
+//     for (int ix = 0; ix < CACHE_SIZE; ix++)
+//         if (cache[ix].valid && cache[ix].key == key)
+//             present = true;
+//     fprintf(stderr, __FILE__ " is_present(" KEY_FMT ") = %s \n", key,
+//             present ? "true" : "false");
+//     return present;
+// }
+
+ValueType _get(KeyType key, bool* found) {
+    for (int ix = 0; ix < CACHE_SIZE; ix++) {
+        if (cache[ix].valid && cache[ix].key == key) {
+            fprintf(stderr, __FILE__ " is_present(" KEY_FMT ") = true\n", key);
+            *found = true;
+            fprintf(stderr, __FILE__ " get(" KEY_FMT ") -> " VALUE_FMT "\n",
+                    key, cache[ix].value);
+            return cache[ix].value;
+        }
+    }
+    fprintf(stderr, __FILE__ " is_present(" KEY_FMT ") = false\n", key);
+    *found = false;
+    return 0;
 }
 
 void _insert(KeyType key, ValueType value) {
     fprintf(stderr, __FILE__ " insert(" KEY_FMT ") = " VALUE_FMT "\n", key,
             value);
-    int ix          = rand() % CACHE_SIZE;
-    cache[ix].key   = key;
-    cache[ix].value = value;
-    cache[ix].valid = true;
-}
 
-ValueType _get(KeyType key) {
-    for (int ix = 0; ix < CACHE_SIZE; ix++)
-        if (cache[ix].valid && cache[ix].key == key) {
-            fprintf(stderr, __FILE__ " get(" KEY_FMT ") -> " VALUE_FMT "\n",
-                    key, cache[ix].value);
-            return cache[ix].value;
-        }
-    return VALUE_NOT_PRESENT;
+    int index;
+    if (current_cache_size < CACHE_SIZE) {
+        // use next available empty slot
+        for (index = 0; index < CACHE_SIZE; index++)
+            if (!cache[index].valid)
+                break;
+        current_cache_size++;
+    } else {
+        // Cache is full, randomly evict
+        index = rand() % CACHE_SIZE;
+    }
+
+    cache[index].key   = key;
+    cache[index].value = value;
+    cache[index].valid = true;
 }
 
 ValueType _caching_provider(KeyType key) {
     cache_requests++;
 
-    if (_is_present(key)) {
+    bool found;
+    ValueType result = _get(key, &found);
+    if (found) {
         cache_hits++;
-        return _get(key);
+        return result;
     }
 
     cache_misses++;
-    ValueType result = (*_downstream)(key);
+    result = (*_downstream)(key);
     _insert(key, result);
-
     return result;
 }
 
