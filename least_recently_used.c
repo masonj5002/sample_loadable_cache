3e87701d7f3483cb9711d8c2c5a62b8fd29b61ac
Mason Jennings <masonj@csu.fullerton.edu>
2025-05-17 03:33:31 -0700

    added least recently used policy

--- least_recently_used.c
+++ least_recently_used.c
@@ -1,7 +1,3 @@
-// Using random_replacement.c as a template
-
-// KNOWN ISSUE: random_replacement.c WRITES TO A RANDOM LOCATION IN THE ARRAY
-// *EVERY TIME* (NOT JUST WHEN THE CACHE IS FULL).
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,25 +5,19 @@
 
 #include "cache.h"
 
-#define CACHE_SIZE 1000
-#define VALUE_NOT_PRESENT 0
-
 typedef struct {
     KeyType key;
     ValueType value;
     bool valid;
-    struct CacheEntry *prev;
-    struct CacheEntry *next;
+    unsigned long long last_used;
 } CacheEntry;
 
-typedef struct {
-    int capacity;
-    int size;
-    CacheEntry *head;
-    CacheEntry *tail;
-} CacheAttributes;
-
+#define CACHE_SIZE 20
 CacheEntry cache[CACHE_SIZE];
+int current_cache_size;
+unsigned long long usage_counter;
+
+#define VALUE_NOT_PRESENT 0
 
 int cache_requests;
 int cache_hits;
@@ -37,10 +27,12 @@ ProviderFunction _downstream = NULL;
 
 void initialize(void) {
     fprintf(stderr, __FILE__ " initialize()\n");
-    srand(time(NULL));
-    cache_requests = 0;
-    cache_hits     = 0;
-    cache_misses   = 0;
+    usage_counter      = 0;
+
+    current_cache_size = 0;
+    cache_requests     = 0;
+    cache_hits         = 0;
+    cache_misses       = 0;
 
     for (int ix = 0; ix < CACHE_SIZE; ix++)
         cache[ix].valid = VALUE_NOT_PRESENT;
@@ -53,11 +45,11 @@ void cleanup(void) {
 CacheStat* statistics(void) {
     fprintf(stderr, __FILE__ " statistics()\n");
 
-    CacheStat* current_stats = malloc(4 * sizeof(CacheStat));
+    CacheStat* current_stats = malloc(5 * sizeof(CacheStat));
     current_stats[0]         = (CacheStat){Cache_requests, cache_requests};
     current_stats[1]         = (CacheStat){Cache_hits, cache_hits};
     current_stats[2]         = (CacheStat){Cache_misses, cache_misses};
-    current_stats[3]         = (CacheStat){Cache_size, CACHE_SIZE};
+    current_stats[3]         = (CacheStat){Cache_size, current_cache_size};
     current_stats[4]         = (CacheStat){END_OF_STATS, 0};
 
     return current_stats;
@@ -70,47 +62,65 @@ void reset_statistics(void) {
     cache_misses   = 0;
 }
 
-bool _is_present(KeyType key) {
-    bool present = false;
-    for (int ix = 0; ix < CACHE_SIZE; ix++)
-        if (cache[ix].valid && cache[ix].key == key)
-            present = true;
-    fprintf(stderr, __FILE__ " is_present(" KEY_FMT ") = %s \n", key,
-            present ? "true" : "false");
-    return present;
+ValueType _get(KeyType key, bool* found) {
+    for (int ix = 0; ix < CACHE_SIZE; ix++) {
+        if (cache[ix].valid && cache[ix].key == key) {
+            fprintf(stderr, __FILE__ " is_present(" KEY_FMT ") = true\n", key);
+            *found              = true;
+            cache[ix].last_used = ++usage_counter; // keeps track of what is being used
+            fprintf(stderr, __FILE__ " get(" KEY_FMT ") -> " VALUE_FMT "\n",
+                    key, cache[ix].value);
+            return cache[ix].value;
+        }
+    }
+    fprintf(stderr, __FILE__ " is_present(" KEY_FMT ") = false\n", key);
+    *found = false;
+    return 0;
 }
 
 void _insert(KeyType key, ValueType value) {
     fprintf(stderr, __FILE__ " insert(" KEY_FMT ") = " VALUE_FMT "\n", key,
             value);
-    int ix          = rand() % CACHE_SIZE;
-    cache[ix].key   = key;
-    cache[ix].value = value;
-    cache[ix].valid = true;
-}
 
-ValueType _get(KeyType key) {
-    for (int ix = 0; ix < CACHE_SIZE; ix++)
-        if (cache[ix].valid && cache[ix].key == key) {
-            fprintf(stderr, __FILE__ " get(" KEY_FMT ") -> " VALUE_FMT "\n",
-                    key, cache[ix].value);
-            return cache[ix].value;
+    int index = -1;
+
+    if (current_cache_size < CACHE_SIZE) {
+        // Find next available slot
+        for (index = 0; index < CACHE_SIZE; index++)
+            if (!cache[index].valid)
+                break;
+        current_cache_size++;
+    } else {
+        // Find least recently used
+        unsigned long long min_usage = ~0ULL;
+        for (int i = 0; i < CACHE_SIZE; i++) {
+            if (cache[i].valid && cache[i].last_used < min_usage) {
+                min_usage = cache[i].last_used;
+                index     = i;
+            }
         }
-    return VALUE_NOT_PRESENT;
+        fprintf(stderr, __FILE__ " Cache is full. Evicting at %d\n", index);
+    }
+
+    cache[index].key       = key;
+    cache[index].value     = value;
+    cache[index].valid     = true;
+    cache[index].last_used = ++usage_counter;
 }
 
 ValueType _caching_provider(KeyType key) {
     cache_requests++;
 
-    if (_is_present(key)) {
+    bool found;
+    ValueType result = _get(key, &found);
+    if (found) {
         cache_hits++;
-        return _get(key);
+        return result;
     }
 
     cache_misses++;
-    ValueType result = (*_downstream)(key);
+    result = (*_downstream)(key);
     _insert(key, result);
-
     return result;
 }
 
